27466b8cecbb1beda00c7895d29c1b90ddcd577c
Joel Ramos <ramosj3@wwu.edu>
2021-07-19 14:13:40 -0700

    made layout responsive and card styles present

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
+/*  
+    use strict is used to declare strict mode, which was designed to be compatible with older
+    versions of JavaScript.
+ 
+    Strict mode makes it easier to write "secure" JavaScript. 
+    https://www.w3schools.com/js/js_strict.asp
+
+    note: this has to be at the top of the js file
+*/
 'use strict';
 
 // what does the require function do exactly
@@ -11,11 +20,11 @@ sass.compiler = require('node-sass')
 // be able to explain what this gulp task does
 gulp.task('sass', () => {
     return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
 })
 
 // be able to explain what this gulp task does
 gulp.task('sass:watch', () => {
-    gulp.watch('./sass/**/*.scss', ['sass'])
+    gulp.watch('./sass/**/*.scss', gulp.series('sass'))
 })
